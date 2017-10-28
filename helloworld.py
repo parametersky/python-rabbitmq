1b821c6144ea7485815774464427b717db87a466
Kyle Cheng <kyle.cheng@live.com>
2017-10-28 16:50:09 +0800

    fix typos

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,7 @@ import pika
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
-channel.basic_public(exchange='',routing_key='hello',body='hello world')
+channel.basic_publish(exchange='',routing_key='hello',body='hello world')
 print(" [x] send 'hello world'")
 connection.close()
 
